23d1fb9ead99ad510df5ed2b93f8647637857df6
kennyakanbi <kennyakanbi@gmail.com>
2024-10-31 15:04:41 +0100

    Week one Assignment

---
+++ user_input.py
@@ -0,0 +1,20 @@
+cler#1. Input fuunction for Name
+name = input("Please enter your name: ")
+print("Hello, " + name + "!")
+
+#2 Input fuunction for age
+age = input("Please enter your age: ")
+print("You are" + age + " years old.")
+
+#3. Input fuunction for Location
+location = input("Please enter your location: ")
+print("You are located in" + location + ".")
+
+#4. Personalized message using tname, age, and location
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
+
